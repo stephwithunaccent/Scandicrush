dbc3a2555537387a53fa2ec0b656a1cb6bc4185b
Stephanie Smith <stephaniecatherine.smith@gmail.com>
2018-03-08 15:28:23 +0100

    npm installement for fancybox

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,5 @@
 class AlbumsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show ]
+  skip_before_action :authenticate_user!, only: [:index, :show, :home ]
 
   def index
     @albums = Album.all
